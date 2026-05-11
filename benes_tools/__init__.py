2546b57fa8618cd5b7eea07c631f56a65e34a80f
Benedikt Buhk <bebuhk@proton.me>
2026-05-11 16:07:55 +0200

    renamed xyz2spherical coord conversion function and extended to handle ints as floats

--- benes_tools/__init__.py
+++ benes_tools/__init__.py
@@ -1,3 +1,3 @@
-from .geometry import to_spherical
+from .geometry import xyz2spherical
 
 __version__ = "0.1.0"
--- benes_tools/geometry.py
+++ benes_tools/geometry.py
@@ -2,17 +2,20 @@ import numpy as np
 
 
 # Spherical coords of each atom in the molecule (used for full sampling rotations)
-def to_spherical(coords, reverse=False, degrees=False):
-    """Convert between Cartesian and spherical coordinates.
+def xyz2spherical(coords, reverse=False, degrees=False):
+    """Convert between Cartesian and spherical coordinates. [N x (x, y, z) -> N x (r, theta, phi)].
     input: 
         coords: array of shape (N, 3) representing N points in either Cartesian (x, y, z) [or spherical (r, theta, phi) coordinates if reverse=True]
         reverse: if False, convert from Cartesian to spherical. If True, convert from spherical to Cartesian.
         degrees: if True, angles (theta, phi) are in degrees. If False, angles are in radians.
 
     output:
-        array of shape (N, 3) representing N points (coords) in the other coordinate system.
+        array of shape (N, 3) representing N points (coords) in the other coordinate system. [default: (r, theta, phi)]
     """
     if not reverse:
+        coords = np.copy(coords)
+        coords = np.float64(coords)  # ensure it's a numpy array (of floats)
+
         out = np.zeros_like(coords)
         for i, (x, y, z) in enumerate(coords):
             r = np.sqrt(x*x + y*y + z*z)
@@ -29,6 +32,5 @@ def to_spherical(coords, reverse=False, degrees=False):
         # convert from spherical to Cartesian (x, y, z) = (r sinθ cosφ, r sinθ sinφ, r cosθ)
         if degrees:
             # input in degrees. convert angles (theta, phi) to radians first
-            coords = np.copy(coords)
             coords[:,1:] = np.radians(coords[:,1:])
         return np.array([[r * np.sin(theta) * np.cos(phi), r * np.sin(theta) * np.sin(phi), r * np.cos(theta)] for r, theta, phi in coords])
